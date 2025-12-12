6f87c9adb405ba3b4c7d2420f8bf3d41ff61887b
Vikum Deshan <vikumdeshan2k01@gmail.com>
2025-12-12 23:19:45 +0530

    Password Hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,20 @@
 import User from '../models/User.js';
+import bcrypt from "bcrypt";
 
 export function createUser(req, res) {
 
     const user = new User(req.body)
 
-    user.save().then(() => {
-        res.json({
-            message: 'User created successfully'
-        });
-    }
-    )
+    const data = req.body;
+
+    const hashedPassword = bcrypt.hashSync(data.password, 10);
+
+    res.json({hashedPassword});
+
+    // user.save().then(() => {
+    //     res.json({
+    //         message: 'User created successfully'
+    //     });
+    // }
+    // )
 }
