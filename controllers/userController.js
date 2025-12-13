d403a258d6f85e26ad324857bdcd6ee80ef469ca
Vikum Deshan <vikumdeshan2k01@gmail.com>
2025-12-13 22:57:02 +0530

    User Login System Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,18 +3,43 @@ import bcrypt from "bcrypt";
 
 export function createUser(req, res) {
 
-    const user = new User(req.body)
-
     const data = req.body;
 
     const hashedPassword = bcrypt.hashSync(data.password, 10);
 
-    res.json({hashedPassword});
+    // res.json({hashedPassword});
 
-    // user.save().then(() => {
-    //     res.json({
-    //         message: 'User created successfully'
-    //     });
-    // }
-    // )
+    const user = new User({
+        email: data.email,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        password: hashedPassword,
+        role: data.role,
+    });
+
+    user.save().then(() => {
+        res.json({
+            message: 'User created successfully'
+        });
+    }
+    )
+}
+
+export function loginUser(req, res) {
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.find({ email: email }).then(
+        (users) => {
+            if (users[0]== null) {
+                res.json({
+                    message: 'User not found'
+                });
+            }else{
+                const user = users[0];
+                res.json(user)
+
+
+
 }
